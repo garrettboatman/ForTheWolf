1691e08f0a1520a6ff22b97818a2db4d77a69882
chu <e1337chu@gmail.com>
2013-06-30 17:04:11 -0400

    Consolidated upload/modify/create extras tools into one file. Haven't tested very well.
    	deleted:    script-modify-success.php
    	modified:   script-upload-success.php
    	modified:   script-upload.php

--- tools/script-upload-success.php
+++ tools/script-upload-success.php
@@ -12,12 +12,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Jake & Amir | Script upload success</title>	
+		<title>Jake & Amir | Script upload/modify success</title>	
 	</head>
 	<body>
-		<a href="script-upload.php"> Back to uploader </a>
+		<a href="script-upload.php"> Back to uploader </a> <br/>
 	
 		<?php
+		/*=================*
+		 *	    UPLOAD     *
+		 *=================*/
+		if(isset($_POST['script-upload'])) {
+		
 			$title = $link = $embedSrc = $duration = $airDate = $scribe = $script = "";
 			$errors = array();
 			
@@ -96,6 +101,113 @@
 				echo "Error(s) detected. The script insertion will not continue until they are fixed. <br>";
 				printErrors($errors);					
 			}
+			
+		}
+		
+		/*=================*
+		 *	    Modify     *
+		 *=================*/
+		else if (isset($_POST['script-modify'])) {
+			$id = $script = "";
+			$errors = array();
+			
+			if($_SERVER["REQUEST_METHOD"] == "POST") {
+									
+				
+				if(!empty($_POST["id"])) {
+					$id =  mysql_real_escape_string ( $_POST["id"] );
+				} else {
+					array_push($errors, "id");
+				}
+				if(!empty($_POST["new-script"])) {
+					//nl2br = newline to break
+					$script = mysql_real_escape_string(nl2br($_POST["new-script"])) ;
+				} else {
+					array_push($errors, "script");
+				}				
+			}
+			
+			echo "id: " . $id . "<br>";
+			echo '<textarea rows="20" cols="40" name="script-result">' . $script . '</textarea>';
+			
+			echo "<br>";
+			
+			if(empty($errors)){
+				echo "Running query: <br>";
+				
+				$updateQuery = 'UPDATE episodes SET script="' . $script . '" WHERE id=' . $id . ';';			
+				$fakeQuery = 'UPDATE episodes SET script="' . '[script]' . '" WHERE id=' . $id . ';';
+				
+				echo $fakeQuery;
+				
+				$con=mysqli_connect("mysql.jakeandamir.dreamhosters.com", "mithos", "martel1864", "janda");
+				if (mysqli_connect_errno($con))	{
+				  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+				mysqli_query($con, "set names utf8");
+				mysqli_query($con, $updateQuery);
+				echo '<br>query completed';
+				
+			} else {
+				echo "Error(s) detected. The script update will not continue until they are fixed. <br>";
+				printErrors($errors);					
+			}
+		}
+		/*=================*
+		 *	    Create     *
+		 *=================*/
+		else if (isset($_POST['create-extra'])) {
+			$id = $name = $link = "";
+			$errors = array();
+			
+			if($_SERVER["REQUEST_METHOD"] == "POST") {
+									
+				
+				if(!empty($_POST["id-extra"])) {
+					$id =  mysql_real_escape_string ( $_POST["id-extra"] );
+				} else {
+					array_push($errors, "id");
+				}				
+				if(!empty($_POST["name-extra"])) {
+					$name =  mysql_real_escape_string ( $_POST["name-extra"] );
+				} else {
+					array_push($errors, "name");
+				}				
+				if(!empty($_POST["link-extra"])) {
+					$link = mysql_real_escape_string($_POST["link-extra"]) ;
+				} else {
+					array_push($errors, "link");
+				}				
+			}
+			
+			echo "id: " . $id . "<br>";
+			echo "name: " . $name . "<br>";
+			echo "link: " . $link . "<br>";
+			
+			echo "<br>";
+			
+			if(empty($errors)){
+				echo "Running query: <br>";
+				
+				$insertionQuery = 'INSERT INTO extras (episode_id, name, link) 
+					VALUES ("' . $id . '", "' . $name . '", "' . $link . '");';		
+						
+						
+				echo $insertionQuery;
+				/*
+				$con=mysqli_connect("mysql.jakeandamir.dreamhosters.com", "mithos", "martel1864", "janda");
+				if (mysqli_connect_errno($con))	{
+				  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+				mysqli_query($con, "set names utf8");
+				mysqli_query($con, $updateQuery);
+				echo '<br>query completed'; */
+				
+			} else {
+				echo "Error(s) detected. The script update will not continue until they are fixed. <br>";
+				printErrors($errors);					
+			}
+		}
 				
 		?>
 		
--- tools/script-upload.php
+++ tools/script-upload.php
@@ -9,39 +9,65 @@
 	</head>
 	<body>
 	
-	<h2>New Script Uploader </h2>
-	
-	<p> 
-		Note: this is for <b>new scripts only</b>. Adding a	
-		script for an existing episode will create a duplicate.
-	</p>
-	
-	<form action="script-upload-success.php" method="post">
-		Title: <input type="text" name="title"> Make sure this matches the CSV file title <i>exactly</i> <br/>
-		Link: <input type="text" name="link"> The link listed in the Reddit master list (e.g. http://www.jakeandamir.com/post/1544860521/toilet-paper )<br/>
-		Embed src: <input type="text" name="embed-src"> (note: do <i>not</i> use an swf for old college humor uploads. Use links of the format http://www.collegehumor.com/e/6338943 )<br/>
-		Duration: <input type="text" name="duration"> e.g 2:25 <br/>
-		Air Date: <input type="text" name="air-date"> e.g. 2013-02-12 <br/>
-		Scribe: <input type="text" name="scribe"> Transcriber's reddit name. Don't leave it blank, find out or put <i>wiki</i><br/>
-		Script:<br> <textarea rows="20" cols="40" name="script"></textarea> The script itself. We may need to edit the text before pasting it in. <br/>
-		<input type="submit">
-	</form>
-	
-	<hr/>
-	
-	<h2>Existing script modifier </h2>
-	
-	<p> 
-		Note: this is for <b>episodes already in the database</b>. 
-		The intent is to update/add a script to an existing episode.
-	</p>
-	
-	<form action="script-modify-success.php" method="post">
+		<h2>New Script Uploader </h2>
 		
-		Id: <input type="text" name="id"> ID of an episode that is already in the database <br/>
-		Script:<br> <textarea rows="20" cols="40" name="new-script"></textarea> The script itself. We may need to edit the text before pasting it in. <br/>
-		<input type="submit">
-	</form>
+		<p> 
+			Note: this is for <b>new scripts only</b>. Adding a	
+			script for an existing episode will create a duplicate.
+		</p>
+		
+		<form action="script-upload-success.php" method="post">
+			Title: <input type="text" name="title"> Make sure this matches the CSV file title <i>exactly</i> <br/>
+			Link: <input type="text" name="link"> The link listed in the Reddit master list (e.g. http://www.jakeandamir.com/post/1544860521/toilet-paper )<br/>
+			Embed src: <input type="text" name="embed-src"> (note: do <i>not</i> use an swf for old college humor uploads. Use links of the format http://www.collegehumor.com/e/6338943 )<br/>
+			Duration: <input type="text" name="duration"> e.g 2:25 <br/>
+			Air Date: <input type="text" name="air-date"> e.g. 2013-02-12 <br/>
+			Scribe: <input type="text" name="scribe"> Transcriber's reddit name. Don't leave it blank, find out or put <i>wiki</i><br/>
+			Script:<br> <textarea rows="20" cols="40" name="script"></textarea> The script itself. We may need to edit the text before pasting it in. <br/>
+			<input type="hidden" name="script-upload" value="1">
+			<input type="submit">
+		</form>
+		
+		<hr/>
+		
+		<h2>Existing script modifier </h2>
+		
+		<p> 
+			Note: this is for <b>episodes already in the database</b>. 
+			The intent is to update/add a script to an existing episode.
+		</p>
+		
+		<form action="script-upload-success.php" method="post">
+			
+			Id: <input type="text" name="id"> ID of an episode that is already in the database <br/>
+			Script:<br> <textarea rows="20" cols="40" name="new-script"></textarea> The script itself. We may need to edit the text before pasting it in. <br/>
+			<input type="hidden" name="script-modify" value="1">
+			<input type="submit">
+		</form>
+		
+		<hr/>
+		
+		<h2>Extras/Outtakes creation </h2>
+		
+		<p> 
+			Note: this is for <b>episodes already in the database</b>. 
+			The intent is to create a new extras entry for an existing episode.
+			You can add as many as you want to a single episode.
+		</p>
+		
+		<p> 
+			You can find this info on the <a href="http://www.reddit.com/r/jakeandamir/comments/11ai05/the_unofficial_ja_master_list/c7hlkoq">Reddit master list </a>
+		</p>
+		
+		<form action="script-upload-success.php" method="post">
+			
+			Episode Id: <input type="text" name="id-extra"> ID of an episode that is already in the database <br/>
+			Name: <input type="text" name="name-extra"> E.g., "Outtakes", "Behind the scenes". Whatever is listed in the <br/>
+			Link:  <input type="text" name="link-extra"> Link to the extra (not an embed) <br/>
+
+			<input type="hidden" name="create-extra" value="1">
+			<input type="submit">
+		</form>
 		
 	</body>	
 </html>
