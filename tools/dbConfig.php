baaaa8bcb1ab574ce6add8798cd5a6a3bc86c2a2
chu <e1337chu@gmail.com>
2013-07-18 17:45:00 -0400

    Added gitignore, hid dbConfig file

--- tools/dbConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostName = "mysql.jakeandamir.dreamhosters.com";
-$userName = "mithos";
-$password = "martel1864";
-$database = "janda";
-
-// Garrett's Local Setup:: $con=mysqli_connect("localhost", "root", "root", "janda");
-?>
