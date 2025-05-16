5db1b703838fda4ebbed2d53f3e4522186d10967
Garrett Boatman <hello@garrettboatman.com>
2025-05-15 21:10:18 -0700

    fix search

--- src/components/SearchContext.tsx
+++ src/components/SearchContext.tsx
@@ -125,7 +125,7 @@ export function SearchProvider({ children }: SearchProviderProps) {
 
     // Update URL with current search parameters
     updateUrl({
-      query,
+      search: query,
     });
 
     try {
