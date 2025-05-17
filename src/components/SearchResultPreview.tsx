8be4e59dcabd592527dd81c25eec9806973b2e52
Reilly Freret <freret@wustl.edu>
2025-05-17 12:34:39 -0400

    displays the first 3 matching lines (with highlighting) when an episode search result is collapsed

---
+++ src/components/SearchResultPreview.tsx
@@ -0,0 +1,48 @@
+import {EpisodeWithHighlight} from "@/utils/types";
+import {useEffect, useState} from "react";
+
+const MAX_PREVIEW_LINES = 3;
+const PREV_CHARACTERS = 10;
+const POST_CHARACTERS = 13;
+const HIGHLIGHT_TAG_START = `<span class="script-match">`;
+const HIGHLIGHT_TAG_END = "</span>";
+
+function parseMatchedChunk(matchedChunk: string): string {
+  const startIndexOfMatch = matchedChunk.indexOf(HIGHLIGHT_TAG_START);
+  const endIndexOfMatch = matchedChunk.indexOf(HIGHLIGHT_TAG_END);
+  const preIndex = Math.max(0, startIndexOfMatch - PREV_CHARACTERS);
+  const postIndex = Math.min(endIndexOfMatch + POST_CHARACTERS, matchedChunk.length);
+  const preEllipsis = preIndex > 0 ? "..." : "";
+  const postEllipsis = postIndex < matchedChunk.length ? "..." : "";
+  return preEllipsis + matchedChunk.substring(preIndex, postIndex) + postEllipsis;
+}
+
+/**
+ * displays a list of highlighted snippets, truncated at both ends. see
+ * @param episode
+ * @constructor
+ */
+export default function SearchResultPreview({episode}: { episode: EpisodeWithHighlight }) {
+  const [highlightedSnippets, setHighlightedSnippets] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (episode.highlight?.script) {
+      const chunks = episode.highlight.script[0].split("<br />");
+      setHighlightedSnippets(
+        chunks.filter(chunk => chunk.includes(HIGHLIGHT_TAG_START))
+          .slice(0, MAX_PREVIEW_LINES)
+          .map(parseMatchedChunk)
+      );
+    }
+  }, [episode]);
+
+  return (
+    <div>
+      <ul>
+        {highlightedSnippets.map((snippet, idx) => (
+          <li key={idx} dangerouslySetInnerHTML={{ __html: snippet }} />
+        ))}
+      </ul>
+    </div>
+  )
+}
